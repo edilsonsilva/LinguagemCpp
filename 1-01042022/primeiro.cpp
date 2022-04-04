57afa1f040936b85a5b45353a1d8004afac0e00a
Edilson Silva <edilsonbackend@gmail.com>
2022-04-04 08:52:33 -0300

    Estudo C++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,19 @@
+/*
+Programa para exibir uma mensagem. O código foi escrito
+em c++
+*/
+/*
+importação da biblioteca de entrada e saída de dados
+*/
+#include <iostream>
+
+int main(){
+
+    //Saída padrão de dados
+    /*
+    cout(console out -> Saída na tela de console(terminal))
+    */
+    std::cout << "Bem Vindo ao C++\n";
+
+    return 0;
+}
